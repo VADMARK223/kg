7662191ca522149b555717857f6ef876e9a8c2b8
v.markitanov <v.markitanov@outlook.com>
2023-03-09 21:01:19 +0300

    Eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    project: ['tsconfig.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    "@typescript-eslint/type-annotation-spacing": "warn", // Предупреждения об отступах
+    "@typescript-eslint/consistent-type-imports": // Типы в импортах
+        [
+          "warn", // Сделать предупреждением
+          {fixStyle: "separate-type-imports"} // Выносить тип за фигурную скобку
+        ]
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Eslint
+
+`npm init @eslint/config` - начальная настройка конфигурации eslint.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
