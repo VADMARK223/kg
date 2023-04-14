572fa3700aabfefb6dd48b458c94ff651034613f
v.markitanov <v.markitanov@outlook.com>
2023-04-14 21:06:55 +0300

    Numbers with bug.

---
+++ src/components/common/KgInput.tsx
@@ -0,0 +1,59 @@
+/**
+ * Компонент ввода с особыми символами
+ *
+ * @author Markitanov Vadim
+ * @since 14.04.2023
+ */
+import React, { useState, useEffect } from 'react'
+import { Input, Button, Space } from 'antd'
+
+interface KgInputProps {
+  inputValueCallback: (value: string) => void
+}
+
+interface SymbolButtonProps {
+  symbol: string
+}
+
+const KgInput = (props: KgInputProps): JSX.Element => {
+  const [value, setValue] = useState<string>('')
+  const [buttonDisable, setButtonDisable] = useState(true)
+
+  useEffect(() => {
+    setButtonDisable(value === '')
+  }, [value])
+
+  const inputSymbolHandler = (symbol: string): void => {
+    const newVal = value + symbol.toLowerCase()
+    setValue(newVal)
+  }
+
+  const SymbolButton = (props: SymbolButtonProps): JSX.Element => {
+    return (
+      <Button type={'primary'} onClick={() => {
+        inputSymbolHandler(props.symbol)
+      }}>{props.symbol}</Button>
+    )
+  }
+
+  const doneHandler = (): void => {
+    props.inputValueCallback(value)
+  }
+
+  return (
+    <Space.Compact>
+      <SymbolButton key={1} symbol={'Ң'}/>
+      <SymbolButton key={2} symbol={'Ү'}/>
+      <SymbolButton key={3} symbol={'Ө'}/>
+      <Input placeholder={'Введите ответ'}
+             allowClear
+             value={value}
+             onChange={(e) => {
+               setValue(e.target.value)
+             }}/>
+      <Button type={'primary'} onClick={doneHandler} disabled={buttonDisable}>Ответить</Button>
+    </Space.Compact>
+  )
+}
+
+export default KgInput
--- src/components/numerals/Numerals.tsx
+++ src/components/numerals/Numerals.tsx
@@ -1,16 +1,142 @@
 /**
- * Компонент
+ * Компонент обучения числам
  *
  * @author Markitanov Vadim
  * @since 11.03.2023
  */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Space, Button, InputNumber } from 'antd'
+import KgInput from '../common/KgInput'
 
 const Numerals = (): JSX.Element => {
+  const [targetNumber, setTargetNumber] = useState<number>(101)
+  const [debug, setDebug] = useState<string>('-')
+  const [rightAnswer, setRightAnswer] = useState<string>('-')
+
+  const convertNumberToString = (value: number): string => {
+    if (value === 0) {
+      return 'нөл'
+    }
+    if (value === 1) {
+      return 'бир'
+    }
+    if (value === 2) {
+      return 'эки'
+    }
+    if (value === 3) {
+      return 'үч'
+    }
+    if (value === 4) {
+      return 'төрт'
+    }
+    if (value === 5) {
+      return 'беш'
+    }
+    if (value === 6) {
+      return 'алты'
+    }
+    if (value === 7) {
+      return 'жети'
+    }
+    if (value === 8) {
+      return 'сегиз'
+    }
+    if (value === 9) {
+      return 'тогуз'
+    }
+    if (value === 10) {
+      return 'он'
+    }
+    if (value === 20) {
+      return 'жыйырма'
+    }
+    if (value === 30) {
+      return 'отуз'
+    }
+    if (value === 40) {
+      return 'кырк'
+    }
+    if (value === 50) {
+      return 'элүү'
+    }
+    if (value === 60) {
+      return 'алтымыш'
+    }
+    if (value === 70) {
+      return 'жетимиш'
+    }
+    if (value === 80) {
+      return 'сексен'
+    }
+    if (value === 90) {
+      return 'токсон'
+    }
+    if (value === 100) {
+      return 'жүз'
+    }
+    if (value === 1000) {
+      return 'миң'
+    }
+    if (value === 1000000) {
+      return 'миллион'
+    }
+    if (value === 1000000) {
+      return 'миллиард'
+    }
+    return 'unknown'
+  }
+
+  useEffect(() => {
+    const numbersArray = String(targetNumber).split('').map(Number)
+    let result: string = ''
+    const resultNumber = []
+    for (let i = numbersArray.length - 1; i >= 0; i--) {
+      result += '|' + convertNumberToString(numbersArray[i])
+      let zero = '1'
+      for (let j = 0; j < numbersArray.length - i - 1; j++) {
+        zero += '0'
+      }
+      const temp: number = Number(zero) * Math.floor(numbersArray[i] * 10 / 10)
+      if (temp !== 0) {
+        resultNumber.push(temp)
+      }
+      result += ' ' + temp
+    }
+    setDebug(result)
+    setRightAnswer(resultNumber.reverse().map(value => convertNumberToString(value)).join(' '))
+  }, [targetNumber])
+
+  const checkResultHandler = (answerString: string): void => {
+    const targetString: string = convertNumberToString(targetNumber)
+    console.log('Целевое число: ', targetString)
+    console.log('Введенное число: ', answerString)
+    if (targetString === answerString) {
+      console.log('GOOD')
+    } else {
+      console.log('BAD')
+    }
+  }
+
   return (
-    <div>
-      Component Numerals
-    </div>
+    <Space direction={'vertical'}>
+      <Space.Compact>
+        <InputNumber<number> value={targetNumber}
+                             min={0}
+                             onChange={(e) => {
+                               if (e != null) {
+                                 setTargetNumber(e)
+                               }
+                             }}/>
+        <Button type={'primary'}>Генерировать</Button>
+      </Space.Compact>
+      <p>
+        Отладка: {debug}
+      </p>
+      <p>
+        Правильный ответ: {rightAnswer}
+      </p>
+      <KgInput inputValueCallback={checkResultHandler}/>
+    </Space>
   )
 }
 
