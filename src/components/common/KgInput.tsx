30f1d8f9119728fb45619fb1df5f3533844083d9
v.markitanov <v.markitanov@outlook.com>
2023-07-21 09:21:50 +0300

    Fix focus.

--- src/components/common/KgInput.tsx
+++ src/components/common/KgInput.tsx
@@ -22,20 +22,24 @@ const KgInput = (props: KgInputProps): JSX.Element => {
   const value = props.valueState[0]
   const setValue = props.valueState[1]
   const [cursorPosition, setCursorPosition] = useState<number | null>(0)
-  const inputRef = useRef<any>()
+  const inputRef = useRef<any | null>()
 
   const SymbolButton = (props: SymbolButtonProps): JSX.Element => {
+    const handlerButton = (event: any): void => {
+      if (inputRef.current != null) {
+        const selectionStart: number = inputRef.current.input.selectionStart
+        setCursorPosition(selectionStart + 1)
+        const newVal = value.slice(0, selectionStart) + props.symbol.toLowerCase() + value.slice(selectionStart)
+        setValue(newVal)
+        inputRef.current.focus()
+      }
+
+      if (event.target != null) {
+        (event.target as HTMLButtonElement).blur()
+      }
+    }
     return (
-      <Button type={'primary'}
-              onFocus={() => {
-                inputRef.current.focus()
-              }}
-              onClick={() => {
-                const selectionStart: number = inputRef.current.input.selectionStart
-                setCursorPosition(selectionStart + 1)
-                const newVal = value.slice(0, selectionStart) + props.symbol.toLowerCase() + value.slice(selectionStart)
-                setValue(newVal)
-              }}>{props.symbol}</Button>
+      <Button type={'primary'} onClick={handlerButton}>{props.symbol}</Button>
     )
   }
 
@@ -56,7 +60,6 @@ const KgInput = (props: KgInputProps): JSX.Element => {
              }}
              onChange={(e) => {
                setValue(e.target.value)
-               // setCursorPosition(e.target.value.length)
                setCursorPosition(null)
              }}/>
     </Space.Compact>
